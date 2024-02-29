b90e24c393867e3b2e6c72468a420e2b4ac4a19b
IAKhaled <ibenameurkhaled@gmail.com>
2024-02-29 16:52:39 +0000

    Uncomment Boundary configuration

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-# output "boundary_token" {
-#   value = vault_token.boundary-token.client_token
-#   sensitive = true
-# }
+output "boundary_token" {
+  value = vault_token.boundary-token.client_token
+  sensitive = true
+}
 
 output "vault_boundary_ssh_ca" {
   value = chomp(data.vault_generic_secret.boundary_ca_public_key.data["public_key"])
